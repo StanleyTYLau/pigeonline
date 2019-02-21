569586d4599b85350f4dbbfa2f60114d81fff461
Stanley <stanley.tyl@gmail.com>
2019-02-21 19:42:48 +0000

    fix reg ex - removed global flag, chartjs added

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -9,25 +9,33 @@ export default {
   name: "ChartVis",
   props: ["userData", "testing"],
   data() {
-    const gmailRegEx = /gmail/gi
-    const yahooRegEx = /yahoo/gi
-    const aolRegEx = /aol/gi
+    const gmailRegEx = /gmail/i
+    const yahooRegEx = /yahoo.com/i
+    const aolRegEx = /aol/i
     let countGmail = 0
     let countYahoo = 0
     let countAol = 0
+    let total = 0
+
     
+    //iterate thru json data and count emails
     for (let user in this.userData){
-      //console.log(this.userData[user].email)
-      if((this.userData[user].email).indexOf("gmail")) {
+      total += 1
+      let email = this.userData[user].email
+
+      if(gmailRegEx.test(email)) {
         countGmail += 1
       }
-      else if (yahooRegEx.test(this.userData[user].email)) {
+      else if (yahooRegEx.test(email)) {
         countYahoo += 1
+        arr.push(email)
       }
-      else if (aolRegEx.test(this.userData[user].email)) {
+      else if (aolRegEx.test(email)) {
         countAol += 1
       }
+
     }
+    
 
     return {
       abc: this.testing + " hello!Gmail: " + countGmail + " Yahoo:" + countYahoo + " Aol:" + countAol
